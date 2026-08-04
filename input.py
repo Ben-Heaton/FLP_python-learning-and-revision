d2579e7e48d4355737e1e22565fa48ff9fa3039d
Ben <benheaton@live.co.uk>
2026-08-04 17:05:42 +0100

    Begun revision

---
+++ input.py
@@ -0,0 +1,6 @@
+# I'm an example of user input
+user = input("I am Python. What is your name?: ")
+print('Welcome', user)
+
+# I'm an example of inputting arguments into the print function to seperate words.
+print("Mary", "had", "a", "little", "lamb", sep=' * ', end="!")
---
+++ var.py
@@ -0,0 +1,23 @@
+# I am a variable assigned an integer value.
+var = 8
+print(var)
+
+# I am a variable assigned a float value.
+var = 3.142
+print(var)
+
+# I am a variable assigned string value.
+var = "Hello there ;)"
+print (var)
+
+# I am a variable assigned a Boolean value.
+var_1 = True
+var_2 = False
+print(var_1, var_2)
+
+"""
+I'm
+a
+multiline
+comment
+"""
