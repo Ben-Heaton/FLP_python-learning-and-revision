7dd8df68561fc0abef22090ca590b4ad1dd34098
Ben <benheaton@live.co.uk>
2026-08-06 17:43:25 +0100

    started chapter 2

---
+++ chapter_2_performing_operations/arithmatic.py
@@ -0,0 +1,24 @@
+a = 8
+b = 2
+print("If a =", a, "and b = ", b)
+
+# Addition
+print("Addition:\t a + b =", a + b)
+
+# Subtraction
+print("Subtraction:\t a - b =", a - b)
+
+# Multiplication
+print("Multiplication:\t a * b =", a * b)
+
+# Division
+print("Division:\t a / b =", a / b)
+
+# Floor Divsion (leaves no floating point)
+print("Floor Division:\t a // b =", a - b)
+
+# Modulo (the remainder)
+print("Modulo:\t\t a % b =", a % b)
+
+# Exponent
+print("Exponent:\t a ** b =", a ** b)
---
+++ chapter_2_performing_operations/assign.py
@@ -0,0 +1,24 @@
+a = 8
+b = 4
+
+print("Assign values:\t\t", "a =", a, "\t\tb =", b)
+
+# Add & Assign
+a += b
+print("Add & Assign:\t\t", "a += b\t\t", a)
+
+# Subtract & Assign
+a -= b
+print("Subtract & Assign:\t", "a -= b\t\t", a)
+
+# Multiply & Assign
+a *= b
+print("Multiply & Assign:\t", "a *= b\t\t", a)
+
+# Divide & Assign
+a /= b
+print("Divide & Assign:\t", "a *= b\t\t", a)
+
+# Modulo & Assign
+a %= b
+print("Modulo & Assign:\t", "a *= b\t\t", a)
