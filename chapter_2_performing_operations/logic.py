c8d0b9c121818a31793b46edd63b354eeb380231
Ben <benheaton@live.co.uk>
2026-08-11 09:40:55 +0100

    did logic

---
+++ chapter_2_performing_operations/logic.py
@@ -0,0 +1,19 @@
+a = True
+b = False
+
+# Both conditions have to agree
+print('AND Logic')
+print("a and a =", a and a)
+print("a and b =", a and b)
+print("b and b =", b and b)
+
+# Either condition has to agree
+print('\nOR Logic')
+print("a or a =", a or a)
+print("a or b =", a or b)
+print("b or b =", b or b)
+
+# the reverse of the condition is true
+print('\nNOT Logic')
+print("a =", not a)
+print("b =", not b)
